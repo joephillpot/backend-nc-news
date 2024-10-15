4c7c25007342f7d15e7fa9ed505630bf69cc3ea6
joephillpot <joephillpot@gmail.com>
2024-10-15 10:00:16 +0100

    Updated error handling for input errors and better handling of custom errors

--- errors.js
+++ errors.js
@@ -1,6 +1,11 @@
+exports.inputErrorHandler = (err, req, res, next) => {
+  res.status(404).send({msg: "Invalid Input"})
+  next(err);
+}
+
 exports.handleCustomErrors = (err, req, res, next) => {
-  if (err.status === 404) {
-    res.status(404).send({ msg: 'Not found' });
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
   } else {
     next(err);
   }

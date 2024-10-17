023cd397d99b2af2065106b69c92c45397746058
joephillpot <joephillpot@gmail.com>
2024-10-17 20:41:13 +0100

    Hosting setup

--- listen.js
+++ listen.js
@@ -1,5 +1,10 @@
 const app = require("./app");
+const {PORT = 9090} = process.env;
 
-app.listen(9090, () => {
-  console.log("Listening on 9090...");
+app.listen(PORT, (err) => {
+  if(err){
+    console.log(err);
+  } else {
+    console.log(`Listening on ${PORT}...`);
+  }
 })

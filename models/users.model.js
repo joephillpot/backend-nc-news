469bffe7dbbfb11772521971c9d3475212c38180
joephillpot <joephillpot@gmail.com>
2024-10-30 21:05:15 +0000

    Added GET: api/users/:username

--- models/users.model.js
+++ models/users.model.js
@@ -5,3 +5,13 @@ exports.fetchUsers = () => {
     return rows;
   });
 };
+
+exports.fetchUserByUsername = (username) => {
+  return db.query(`SELECT * FROM users WHERE username = $1`, [username])
+  .then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: 'Not found' });
+    }
+    return rows[0];
+  });
+};
--- routers/users.router.js
+++ routers/users.router.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getUsers } = require('../controllers/users.controller');
+const { getUsers, getUserByUsername } = require('../controllers/users.controller');
 const router = express.Router();
 
 router.get("/", getUsers)
+router.get("/:username", getUserByUsername)
 
 module.exports = router;
